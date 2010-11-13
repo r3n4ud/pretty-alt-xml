86ac8f59737fd5721fa7658edba07ccc1bb8b2a3
Renaud AUBIN <root@renaud.io>
2010-11-13 19:29:39 +0100

    First operational version.

--- pret-xml.lua
+++ pret-xml.lua
@@ -1,11 +1,30 @@
 if not modules then modules = { } end modules ['pret-xml'] = {
     version   = 1.001,
-    comment   = "Time-stamp: <2010-11-08 00:10:12>",
+    comment   = "Time-stamp: <2010-11-13 19:25:18>",
     author    = "Renaud AUBIN",
     copyright = "Renaud AUBIN",
     license   = ""
 }
 
+
+-- TODO:
+-- − Simplify with functors
+-- − Improve recurse_attribute (detect invalid, detect =)
+-- − Use oxygen xml -like color scheme
+--    0 0 0        --  1 Text
+--    0 100 0      --  2 Comment
+--    0 0 255      --  3 Doctype
+--    153 51 0     --  4 Quoted Value
+--    153 51 0     --  5 Single quoted Value
+--    150 150 0    --  6 Entity
+--    0 0 150      --  7 Tag
+--    245 132 76   --  8 Attribute name
+--    139 38 201   --  9 Processing Instruction
+--    255 128 64   -- 10 Equal
+--    255 0 0      -- 11 Invalid
+--    0 140 0      -- 12 CDATA
+--    139 38 201   -- 13 XML prolog
+-- − Clean up unused variables
 
 local utf = unicode.utf8
 local utfcharacters, utfvalues = string.utfcharacters, string.utfvalues
@@ -21,19 +40,6 @@ local visualizer = buffers.newvisualizer("xml")
 
 local match = string.match
 
--- 0 0 0        --  1 Text
--- 0 100 0      --  2 Comment
--- 0 0 255      --  3 Doctype
--- 153 51 0     --  4 Quoted Value
--- 153 51 0     --  5 Single quoted Value
--- 150 150 0    --  6 Entity
--- 0 0 150      --  7 Tag
--- 245 132 76   --  8 Attribute name
--- 139 38 201   --  9 Processing Instruction
--- 255 128 64   -- 10 Equal
--- 255 0 0      -- 11 Invalid
--- 0 140 0      -- 12 CDATA
--- 139 38 201   -- 13 XML prolog
 
 local colors = {
    "prettyone",
@@ -55,9 +61,13 @@ local states = {
    ["text"]=1,
    ["Comment"]=2,
    ["Doctype"]=3,
+   ["Quoted"]=4,
+   ["Entity"]=5,
+   ["AttributeName"]=6,
    ["Tag"]=7,
-   ["CDATA"]=12,
-   ["XmlProlog"]=13,
+   ["Invalid"]=8,
+   ["CDATA"]=9,
+   ["XmlProlog"]=10,
 }
 
 local state, line_number, startText, inText, stopText, startComment, inComment, stopComment, startDoctype, inDoctype, stopDoctype, startQuotedValue, inQuotedValue, stopQuotedValue, startSingleQuotedValue, inSingleQuotedValue, stopSingleQuotedValue, startEntity, inEntity, stopEntity, startTag, inTag, stopTag, startAttributeName, inAttributeName, stopAttributeName, startProcessingInstruction, inProcessingInstruction, stopProcessingInstruction, Equal, Invalid, startCDATA, inCDATA, stopCDATA, startXmlProlog, inXmlProlog, stopXmlProlog
@@ -87,7 +97,7 @@ function visualizer.reset()
    inTag = false
    stopTag = false
    startAttributeName = false
-   inAttributeName = false
+   inAttributeName = true
    stopAttributeName = false
    startProcessingInstruction = false
    inProcessingInstruction = false
@@ -111,10 +121,90 @@ local function flush_text(str)
 end
 
 local before, capture, after
+local attrbefore, attrcapture, attrafter
+
+local function recurse_attribute(str)
+   if (string.len(str)==0) then
+      return
+   elseif (startSingleQuotedValue or inSingleQuotedValue) then
+      if (match(str,"(.-')(.*)")) then
+         -- stopSingleQuotedValue
+         attrcapture, str = match(str,"(.-')(.*)")
+         state = changestate(states["Quoted"], state)
+         flush_text(attrcapture)
+         state = finishstate(state)
+         startSingleQuotedValue=false
+         inSingleQuotedValue=false
+         recurse_attribute(str)
+      else
+         -- inSingleQuotedValue
+         inSingleQuotedValue=true
+         state = changestate(states["Quoted"], state)
+         flush_text(str)
+         state = finishstate(state)
+      end
+   elseif (startQuotedValue or inQuotedValue) then
+      if (match(str,"(.-\")(.*)")) then
+         -- stopQuotedValue
+         attrcapture, str = match(str,"(.-\")(.*)")
+         state = changestate(states["Quoted"], state)
+         flush_text(attrcapture)
+         state = finishstate(state)
+         startQuotedValue=false
+         inQuotedValue=false
+         recurse_attribute(str)
+      else
+         -- inQuotedValue
+         inQuotedValue=true
+         state = changestate(states["Quoted"], state)
+         flush_text(str)
+         state = finishstate(state)
+      end
+   elseif (match(str,".-'.*")) then
+      -- startSingleQuotedValue
+      attrbefore, attrcapture, attrafter = match(str,"(.-)(')(.*)")
+      recurse_attribute(attrbefore)
+      state = changestate(states["Quoted"], state)
+      flush_text(attrcapture)
+      state = finishstate(state)
+      startSingleQuotedValue = true
+      recurse_attribute(attrafter)
+   elseif (match(str,".-\".*")) then
+      -- startQuotedValue
+      attrbefore, attrcapture, attrafter = match(str,"(.-)(\")(.*)")
+      recurse_attribute(attrbefore)
+      state = changestate(states["Quoted"], state)
+      flush_text(attrcapture)
+      state = finishstate(state)
+      startQuotedValue = true
+      recurse_attribute(attrafter)
+   else
+      state = changestate(states["AttributeName"], state)
+      flush_text(str)
+      state = finishstate(state)
+      str=""
+   end
+end
 
 local function recurse(str)
    if (string.len(str)==0) then
       return
+   elseif (startTag or inTag) then
+      if (match(str,".-/?>.*")) then
+         -- stopTag
+         before, capture, str = match(str,"(.-)(/?>)(.*)")
+         recurse_attribute(before)
+         state = changestate(states["Tag"], state)
+         flush_text(capture)
+         state = finishstate(state)
+         startTag=false
+         inTag=false
+         recurse(str)
+      else
+         -- inTag
+         inTag=true
+         recurse_attribute(str)
+      end
    elseif (startXmlProlog or inXmlProlog) then
       if (match(str,"(.-%?>)(.*)")) then
          -- stopXmlProlog
@@ -183,6 +273,15 @@ local function recurse(str)
          flush_text(str)
          state = finishstate(state)
       end
+   elseif (match(str,"(.-)(<%a+%s-)(.*)")) then
+      -- startTag
+      before, capture, after = match(str,"(.-)(<%a+%s-)(.*)")
+      recurse(before)
+      startTag = true
+      state = changestate(states["Tag"], state)
+      flush_text(capture)
+      state = finishstate(state)
+      recurse(after)
    elseif (match(str,"(.-)(<%?)(.*)")) then
       -- startXmlProlog
       before, capture, after = match(str,"(.-)(<%?)(.*)")
@@ -219,46 +318,29 @@ local function recurse(str)
       flush_text(capture)
       state = finishstate(state)
       recurse(after)
+   elseif (match(str,"(.-)(&%a+;)(.*)")) then
+      -- Entity (atomic operation / i.e. no multiline treatment)
+      before, capture, after = match(str,"(.-)(&%a+;)(.*)")
+      recurse(before)
+      state = changestate(states["Entity"], state)
+      flush_text(capture)
+      state = finishstate(state)
+      recurse(after)
+   elseif (match(str,"(.-)(</%a+>)(.*)")) then
+      -- Closing Tag (atomic operation / i.e. no multiline treatment)
+      before, capture, after = match(str,"(.-)(</%a+>)(.*)")
+      recurse(before)
+      state = changestate(states["Tag"], state)
+      flush_text(capture)
+      state = finishstate(state)
+      recurse(after)
    else
       flush_text(str)
       str=""
    end
 end
 
-local line_number = 0
-
 function visualizer.flush_line(str,nested)
    buffers.currentcolors = colors
-   line_number = line_number + 1
-   texio.write(line_number.." ")
    recurse(str)
-   -- local startprolog = match(str,"(%s*<%?.-%?>%s*)")
-   -- local doctype = match(str,"(%s*<!DOCTYPE.->%s*)") -- todo multiline
-   -- local cdata   = match(str,"(%s*<!%[.-%]>%s*)") -- todo multiline
-   -- local tag = match(str,"(%s*</?.->%s*)")
-
-   -- if (prolog) then
-   --    texio.write_nl("XML Prolog")
-   --    state = changestate(states["XmlProlog"] ,state)
-   --    flush_text(prolog)
-   --    state = finishstate(state)
-   -- elseif (doctype) then
-   --    texio.write_nl("DOCTYPE")
-   --    state = changestate(states["Doctype"] ,state)
-   --    flush_text(doctype)
-   --    state = finishstate(state)
-   -- elseif (cdata) then
-   --    texio.write_nl("CDATA")
-   --    state = changestate(states["CDATA"] ,state)
-   --    flush_text(cdata)
-   --    state = finishstate(state)
-   -- elseif (tag) then
-   --    texio.write_nl("Tag")
-   --    state = changestate(states["Tag"] ,state)
-   --    flush_text(tag)
-   --    state = finishstate(state)
-   -- else
-   --    texio.write_nl("NO PROCESSING")
-   -- end
-   texio.write_nl("")
 end
