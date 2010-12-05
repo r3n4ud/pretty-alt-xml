adf4a2f76f7b22fdd26f6757eb8414d662cac221
Renaud AUBIN <root@renaud.io>
2010-12-05 14:04:57 +0100

    Copyright added.

--- pret-xml.lua
+++ pret-xml.lua
@@ -1,9 +1,24 @@
+-- Copyright 2010 Renaud Aubin <renaud.aubin@gmail.com>
+-- Time-stamp: <2010-12-05 14:04:12>
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if not modules then modules = { } end modules ['pret-xml'] = {
-    version   = 0.100,
+    version   = 0.1,
     comment   = "Time-stamp: <2010-11-20 19:31:14>",
     author    = "Renaud Aubin",
-    copyright = "Renaud Aubin",
-    license   = "see context related readme file"
+    copyright = "2010 Renaud Aubin",
+    license   = "GNU General Public License version 3"
 }
 
 --[[
